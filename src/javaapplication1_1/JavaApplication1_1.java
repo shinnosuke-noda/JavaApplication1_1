be19006134bf7d713381f73fd4a9c886be963ff7
shinnode <shinnosuke.noda@mail.utoronto.ca>
2022-04-30 01:08:18 -0400

    First commit

---
+++ src/javaapplication1_1/JavaApplication1_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1_1;
+
+/**
+ *
+ * @author Shin
+ */
+public class JavaApplication1_1 {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+    
+}
